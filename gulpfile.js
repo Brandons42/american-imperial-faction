4ee2b522e51d3b7ba5b4e8c56f318beef967cd9e
Brandon <brandonbsuen@gmail.com>
2019-10-03 22:48:04 -0700

    Majority of content and styles implemented

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const { dest, parallel, series, src, watch } = require('gulp');
+const cleanCSS = require('gulp-clean-css');
+const ghPages = require('gulp-gh-pages');
+const htmlmin = require('gulp-htmlmin');
+const imagemin = require('gulp-imagemin');
+
+const clean = () => del('./.publish/', { force: true });
+
+const deploy = () => src('./dist/**/*').pipe(ghPages());
+
+const images = () =>
+	src('./images/*')
+		.pipe(imagemin())
+		.pipe(dest('./dist/images/'));
+
+const markup = () =>
+	src('./index.html')
+		.pipe(htmlmin())
+		.pipe(dest('./dist/'));
+
+const styles = () =>
+	src('./index.css')
+		.pipe(cleanCSS())
+		.pipe(dest('./dist/'));
+
+const videos = () => src('./videos/*').pipe(dest('./dist/videos/'));
+
+const build = parallel(images, markup, styles, videos);
+
+exports.default = build;
+
+exports.deploy = series(build, deploy, clean);
+
+exports.dev = () => {
+	watch('./images/*', images);
+	watch('./index.html', markup);
+	watch('./index.css', styles);
+	watch('./videos/*', videos);
+};
